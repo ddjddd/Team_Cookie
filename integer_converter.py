7943e1510b2b1f4539ebb29740a321ff5df4ddd1
jinho <ddjddd@naver.com>
2018-04-08 18:06:27 +0900

    split into package

--- integer_converter.py
+++ integer_converter.py
@@ -1,11 +1,11 @@
 import cv2
-import bluestack_api
 
 tpl_name_list = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'B']
 tpl_contour_list = []
 
 
-def read_template():        # 템플릿 읽어오기 (resources 폴더에 저장)
+# 템플릿 읽어오기 (resources 폴더에 저장)
+def read_template():
     global tpl_name_list, tpl_contour_list
 
     for file in tpl_name_list:
@@ -27,7 +27,8 @@ def read_template():        # 템플릿 읽어오기 (resources 폴더에 저장
                     rx = x          # 기준치 검사를 위한 변수 저장
 
 
-def health2int(health_frame, height, width):        # 체력 이미지에서 체력 값을 정수로 반환하는 함수
+# 체력 이미지에서 체력 값을 정수로 반환하는 함수
+def health2int(health_frame, height, width):
 
     # 이미지에서 외곽선 찾기위한 필터 적용
     health_blur = cv2.GaussianBlur(health_frame, (3, 3), 0)     # 가우시안 블러 효과 적용
@@ -44,7 +45,8 @@ def health2int(health_frame, height, width):        # 체력 이미지에서 체
     return ret_health
 
 
-def score2int(score_frame):     # 점수 이미지에서 점수 값을 정수로 반환하는 함수
+# 점수 이미지에서 점수 값을 정수로 반환하는 함수
+def score2int(score_frame):
 
     # 이미지에서 외곽선 찾아내기 위한 필터 적용
     score_gray = cv2.cvtColor(score_frame, cv2.COLOR_BGR2GRAY)  # 흑백 이미지 전환
@@ -120,7 +122,8 @@ def score2int(score_frame):     # 점수 이미지에서 점수 값을 정수로
     return score_result, judge
 
 
-def get_score_size(height, width):          # 점수 표시 부분의 사이즈 설정
+# 점수 표시 부분의 사이즈 설정
+def get_score_size(height, width):
     sx1 = int(width * 70 / 100)
     sx2 = int(width * 95 / 100)
     sy1 = int(height * 1 / 100)
@@ -128,46 +131,10 @@ def get_score_size(height, width):          # 점수 표시 부분의 사이즈 
     return sx1, sx2, sy1, sy2
 
 
-def get_health_size(height, width):         # 체력 표시 부분의 사이즈 설정
+# 체력 표시 부분의 사이즈 설정
+def get_health_size(height, width):
     hx1 = int(width * 7 / 100)
     hx2 = int(width * 90 / 100)
     hy1 = int(height * 10 / 100)
     hy2 = int(height * 15 / 100)
     return hx1, hx2, hy1, hy2
-
-
-def main():        # 메인 함수
-    global tpl_name_list, tpl_contour_list          # 전역 변수로 설정
-    video = cv2.VideoCapture('resources/Examples/cookierun.mp4')
-    read_template()         # 템플릿 읽기
-
-    recent_score = 0
-    while True:
-        ret, frame = video.read()               # 동영상 입력 받기
-        height, width, channels = frame.shape   # 동영상의 크기 입력
-
-        # 이미지에서 점수 추출하기
-        sx1, sx2, sy1, sy2 = get_score_size(height, width)      # 점수표시부분 크기 추출
-        score_frame = frame[sy1:sy2, sx1:sx2]                   # 점수 표시 부분만 잘라내기
-        score, judge = score2int(score_frame)                   # 이미지->정수로 변환
-        if judge is not True:           # 점수 컨투어의 맨 왼쪽 컨투어가 젤리가 아닌 경우
-            score = recent_score        # 이전의 점수를 그대로 가져오기
-        recent_score = score            # 현재 점수를 다음 프레임으로 전달하기 위한 벼눗 저장
-
-        # 이미지에서 체력 추출하기
-        hx1, hx2, hy1, hy2 = get_health_size(height, width)     # 체력표시부분 크기 추출
-        health_frame = frame[hy1:hy2, hx1:hx2]                  # 체력 표시 부분만 잘라내기
-        health = health2int(health_frame, height, width)        # 이미지->체력으로 변환
-
-        # 이미지에 변환된 값들 출력
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(frame, str(health), (20, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)     # 체력 출력
-        cv2.putText(frame, str(score), (550, 30), font, 1, (255, 255, 255), 2, cv2.LINE_AA)     # 점수 출력
-
-        cv2.imshow('Cookie', frame)
-        cv2.waitKey(10)
-
-    video.release()
-    cv2.destroyAllWindows()
-
-main()
