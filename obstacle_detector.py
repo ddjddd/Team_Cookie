3a5c7c33bcbc36671bbe561647fe7484715826ca
jinho <ddjddd@naver.com>
2018-04-29 22:16:18 +0900

    Add 'Merging contour method'

--- obstacle_detector.py
+++ obstacle_detector.py
@@ -9,26 +9,59 @@ def get_roi_window_size(hgt, wth):
     return wx1, wx2, wy1, wy2
 
 
+def lister(list):
+    list.sort()
+    for i in range(len(list)):
+        j = i + 1
+        while j < len(list):
+            merge = False
+            if list[i][2] >= list[j][0]:
+                if list[i][1] <= list[j][1] <= list[i][3]:
+                    list[i][2] = max(list[i][2], list[j][2])
+                    list[i][3] = max(list[i][3], list[j][3])
+                    del list[j]
+                    merge = True
+                elif list[i][1] <= list[j][3] <= list[i][3]:
+                    list[i][2] = max(list[i][2], list[j][2])
+                    list[i][1] = min(list[i][1], list[j][1])
+                    del list[j]
+                    merge = True
+                elif list[i][1] >= list[j][1] and list[i][3] <= list[j][3]:
+                    list[i][2] = max(list[i][2], list[j][2])
+                    list[i][1] = list[j][1]
+                    list[i][3] = list[j][3]
+                    del list[j]
+                    merge = True
+
+            if merge is False:
+                j += 1
+            else:
+                j = i + 1
+
+
 def detector(roi_frame, height, width):
     roi_blur = cv2.GaussianBlur(roi_frame, (3, 3), 0)
     roi_canny = cv2.Canny(roi_blur, 170, 200)
-    rx = 10000
+    obstacle_list = []
 
-    ob_threshhold = int(height * 93 / 100)
-
-    cv2.line(roi_frame, (0, ob_threshhold), (width, ob_threshhold), (255, 255, 255), 3)
     _, roi_contours, roi_hierarchy = cv2.findContours(roi_canny, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     for i in range(len(roi_contours)):
         x, y, w, h = cv2.boundingRect(roi_contours[i])
         rect_area = w * h
-        aspect_ratio = float(w) / h
         if roi_hierarchy[0][i][3] is not -1:
-            # cv2.rectangle(roi_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
             if rect_area >= 550:
-                if y+h >= ob_threshhold :
-                    cv2.rectangle(roi_frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                obstacle_list.append([x, y, x+w, y+h])
 
-    cv2.imshow('obs', roi_canny)
-    cv2.imshow('Obstacles', roi_frame)
-    # cv2.destroyAllWindows()
+    lister(obstacle_list)
+
+    for i in range(len(obstacle_list)):
+        x, y, w, h = obstacle_list[i]
+        if y <= 10:
+            cv2.rectangle(roi_frame, (x, y), (w, h), (0, 0, 255), 2)
+        elif h >= height - 13:
+            cv2.rectangle(roi_frame, (x, y), (w, h), (0, 0, 255), 2)
+        else:
+            cv2.rectangle(roi_frame, (x, y), (w, h), (0, 255, 0), 2)
+
+
     return
