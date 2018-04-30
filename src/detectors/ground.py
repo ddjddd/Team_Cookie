6c7b8b5ee966397d70be75e425978e0c114a70cc
ddjddd <ddjddd@naver.com>
2018-04-30 19:31:34 +0900

    Add notes

--- src/detectors/ground.py
+++ src/detectors/ground.py
@@ -1,9 +1,9 @@
 import cv2
-from src import image_filter as imf
+from src import image_filter as imf, window_size as ws
 from src.detectors import collision_detector as cd
-from src import window_size as ws
 
 
+# 인접한 오브젝트를 묶어 하나의 오브젝트로 만든다
 def make_ground_list(ground_list):
     ground_list.sort()
     thr = 40
@@ -23,11 +23,14 @@ def make_ground_list(ground_list):
                 j = i + 1
 
 
+# 바닥 오브젝트를 검출
 def ground(frame, hgt, wth):
+    # 바닥 화면만을 우선적으로 검사
     gx1, gx2, gy1, gy2 = ws.get_ground_size(hgt, wth)
     ground_frame = frame[gy1:gy2, gx1:gx2]
     binary = imf.make_canny(ground_frame)
 
+    # 외곽선 검출
     _, contours, roi_hierarchy = cv2.findContours(binary, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     ground_list = []
     for i in range(len(contours)):
@@ -37,16 +40,14 @@ def ground(frame, hgt, wth):
             if rect_area >= 250:
                 ground_list.append([x+gx1, y+gy1, x+w+gx1, y+h+gy1])
 
-    make_ground_list(ground_list)
+    make_ground_list(ground_list)           # 인접한 바닥 오브젝트 묶기
 
-    for i in range(len(ground_list)):
-        x, y, w, h = ground_list[i]
-        # ground_list[i][0] += gx1
-        # ground_list[i][2] += gx1
-        # ground_list[i][1] += gy1
-        # ground_list[i][3] += gy1
-        cv2.rectangle(frame, (x, y), (w, h), (255, 0, 0), 2)
 
-    cd.ground_collision(frame, ground_list)
+    # 사각형 그리는 함수    # 현재 비활성화
+    # for i in range(len(ground_list)):
+    #     x, y, w, h = ground_list[i]
+    #     cv2.rectangle(frame, (x, y), (w, h), (255, 0, 0), 2)
+
+    cd.ground_collision(frame, ground_list)     # 바닥-그리드 충돌 검사 및 그리드 채우기
 
     return
